ec7a931408e846abaa3492ee074ae2981a6d5320
AEBU <b_enriq@yahoo.com>
2017-08-29 09:44:42 -0500

    :LoginActivityView

---
+++ app/src/main/java/ec/uce/lex/androidchatproject/contactlist/ContactListActivity.java
@@ -0,0 +1,15 @@
+package ec.uce.lex.androidchatproject.contactlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ec.uce.lex.androidchatproject.R;
+
+public class ContactListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
--- app/src/main/java/ec/uce/lex/androidchatproject/login/LoginActivity.java
+++ app/src/main/java/ec/uce/lex/androidchatproject/login/LoginActivity.java
@@ -1,25 +1,39 @@
 package ec.uce.lex.androidchatproject.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import ec.uce.lex.androidchatproject.R;
+import ec.uce.lex.androidchatproject.contactlist.ContactListActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginView {
 
-    @BindView(R.id.editTxtEmail)       EditText inputEmail;
-    @BindView(R.id.btnSignin)          Button btnSignin;
-    @BindView(R.id.btn_Signup)         Button btnSignup;
-    @BindView(R.id.editTxtPassword)    EditText inputPassword;
-    @BindView(R.id.progressBar)        ProgressBar progressBar;
+    @BindView(R.id.editTxtEmail)
+    EditText inputEmail;
+    @BindView(R.id.btnSignin)
+    Button btnSignIn;
+    @BindView(R.id.btn_Signup)
+    Button btnSignUp;
+    @BindView(R.id.editTxtPassword)
+    EditText inputPassword;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.activity_main)
+    RelativeLayout container;
+
+
+    private LoginPresenter loginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +46,81 @@ public class LoginActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnSignin:
-                Log.e("Signin",inputEmail.getText().toString());
+                Log.e("Signin", inputEmail.getText().toString());
                 break;
             case R.id.btn_Signup:
-                Log.e("Signup",inputPassword.getText().toString());
+                Log.e("Signup", inputPassword.getText().toString());
                 break;
         }
+    }
+
+    @Override
+    public void enableInputs() {
+        setInputs(true);
+    }
+
+    @Override
+    public void disableInputs() {
+        setInputs(false);
+    }
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.btn_Signup)
+    @Override
+    public void handleSignUp() {
+        loginPresenter.registerNewUser(inputEmail.getText().toString(),
+                inputPassword.getText().toString());
+    }
+
+
+    @OnClick(R.id.btnSignin)
+    @Override
+    public void handleSignIn() {
+        loginPresenter.validateLogin(inputEmail.getText().toString(),
+                inputPassword.getText().toString());
+    }
+
+    @Override
+    public void navigateToMainScreen() {
+        startActivity(new Intent(this, ContactListActivity.class));
+    }
+
+    @Override
+    public void loginError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signin), error);
+        inputPassword.setError(msgError);
+    }
+
+    @Override
+    public void newUserSuccess() {
+        Snackbar.make(container, R.string.login_notice_message_signup, Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void newUserError(String error) {
+        inputPassword.setText("");
+        String msgError = String.format(getString(R.string.login_error_message_signup), error);
+        inputPassword.setError(msgError);
+    }
+
+
+    /*
+    * @parameter enabled para ver si esta habilitado o no
+    * */
+    private void setInputs(boolean enabled) {
+        btnSignIn.setEnabled(true);
+        btnSignUp.setEnabled(true);
+        inputEmail.setEnabled(true);
+        inputPassword.setEnabled(true);
     }
 }
